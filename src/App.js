ca9a278c0d94a58d691299d799608eafb34a43d4
amer marei <amermarei730@yahoo.com>
2022-12-21 05:31:59 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+
+import './App.css';
+import { Login } from './Component/Login';
+import { Register } from './Component/Register';
+import { Home } from './Component/Home';
+import { Navbar } from './Component/Navbar';
+import  Layout  from './Component/Layout';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import ParticlesComponent from './Component/Partices';
+
+
+let routers= createBrowserRouter([
+  {path:'/', element:<Layout/>,children:[
+    {index:"true", element:<Login/>},
+    {path:"register", element:<Register/>},
+    {path:"home", element:<Home/>},
+    {path:"*", element:<Login/>},
+  ]}
+])
+
+
+function App() {
+  return <>
+      
+     
+      <RouterProvider router={routers}/>
+      
+      <ParticlesComponent/>
+      
+  </>
+      
+       
+    
+
+}
+
+export default App;
